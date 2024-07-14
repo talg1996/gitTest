5d2b31a16925a9d1c79a03d780f5e8feecc735e0
tal.gadasi <tal.gadasi@TAL-GADASI-LP>
2024-07-14 11:46:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Master");
+Console.WriteLine("Master2");
+Console.WriteLine("Master3");
+
+Console.WriteLine("Slave1");
+
+
+Console.WriteLine("Slave22");
+
+
+Console.WriteLine("Slave3");
+
+
+
