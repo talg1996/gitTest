3045644de321e110796de3f6d2b04560233c2430
tal.gadasi <tal.gadasi@TAL-GADASI-LP>
2024-07-14 11:47:46 +0300

    first master commit

--- Program.cs
+++ Program.cs
@@ -3,13 +3,5 @@ Console.WriteLine("Master");
 Console.WriteLine("Master2");
 Console.WriteLine("Master3");
 
-Console.WriteLine("Slave1");
-
-
-Console.WriteLine("Slave22");
-
-
-Console.WriteLine("Slave3");
-
 
 
